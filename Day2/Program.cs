133467d0904642354ab83afee3c6c8187332212d
Emaity <matthew.ewhs@gmail.com>
2024-12-04 16:39:39 -0500

    Day2 Part 2 attempt

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -3,6 +3,12 @@
 string? line;
 int numSafe = 0;
 
+// Receives numbers in the order that they appear in the report
+static bool isSafe(int first, int second, bool increasing) {
+    // inverse of the conditions that would make a report unsafe: wrong increase/decrease, large change, same number
+    return !(first == second || increasing && first > second || !increasing && first < second || Math.Abs(first - second) > 3);
+}
+
 try {
     StreamReader sr = new StreamReader("input.txt");
     line = sr.ReadLine();
@@ -10,22 +16,35 @@ try {
     while(line != null) {
         int[] nums = Array.ConvertAll(line.Split(" "), int.Parse);
 
-        bool increasing = false;
-        if(nums.Length > 1 && nums[1] > nums[0]) {
-            increasing = true;
-        }
+        // Compares the last and first indices to check if a report should be increasing
+        // If the first or last numbers are ones that should be removed, then this is bad lol I'm just hoping it works though
+        // We could instead possibly look through every number and choose the greater between observed increases or decreases?
+        bool increasing = nums[nums.Length-1] > nums[0] ? true : false;
 
         bool safe = true;
+        int numSkips = 0;
         for(int i=1 ; i<nums.Length ; i++) {
-            // if decreasing when should be increasing, or if change too fast or nums the same
-            if(increasing && nums[i] < nums[i-1] || !increasing && nums[i] > nums[i-1] || Math.Abs(nums[i] - nums[i-1]) > 3 || nums[i] == nums[i-1]) {
+            if( !isSafe(nums[i-1], nums[i], increasing) ) {
                 safe = false;
-                break;
+
+                // If two adjacent numbers are not safe, then check the one after
+                i++; // bad practice to increment i in for loop
+                if(i < nums.Length && isSafe(nums[i-2], nums[i], increasing)) {
+                    numSkips++;
+                }
             }
         }
 
-        if(safe)
+        if(safe || (!safe && numSkips == 1))
             numSafe++;
+
+        foreach(int num in nums) {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+        Console.Write($"safe: {safe}, skips: {numSkips}, increasing: {increasing}");
+        Console.WriteLine();
+        Console.WriteLine();
             
         line = sr.ReadLine();
     }
