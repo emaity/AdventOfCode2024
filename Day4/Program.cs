79179f7883e109ea7a0876d489245c9f8d9ab5d2
emaity <matthew.ewhs@gmail.com>
2024-12-08 15:39:27 -0500

    Day 4 poor solution

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System.IO;
 
 bool findXmasHelper(int r, int c, int pos, Direction dir, char[,] wordSearch, int rowLen, int colLen) {
+    if(pos > 3)
+        return true;
     if(r < 0 || r>=rowLen || c<0 || c>=colLen) {
         return false;
     }
-    if(pos > 3)
-        return true;
 
     // Check current letter
     char curr = wordSearch[r,c];
@@ -43,7 +43,7 @@ bool findXmasHelper(int r, int c, int pos, Direction dir, char[,] wordSearch, in
         case Direction._SW:
             return findXmasHelper(r+1, c-1, pos+1, Direction._SW, wordSearch, rowLen, colLen);
         case Direction._W:
-            return findXmasHelper(r, c-1, pos+1, Direction._E, wordSearch, rowLen, colLen);
+            return findXmasHelper(r, c-1, pos+1, Direction._W, wordSearch, rowLen, colLen);
         case Direction._NW:
             return findXmasHelper(r-1, c-1, pos+1, Direction._NW, wordSearch, rowLen, colLen);
         default:
@@ -57,56 +57,96 @@ int findXmas(int r, int c, char[,] wordSearch, int rowLen, int colLen) {
 
     if(r>0) {
         // NW
-        if(c>0 && wordSearch[r-1,c-1] == 'M' && findXmasHelper(r-1, c-1, 1, Direction._NW, wordSearch, rowLen, colLen))
+        if(c>0 && wordSearch[r-1,c-1] == 'M' && findXmasHelper(r-1, c-1, 1, Direction._NW, wordSearch, rowLen, colLen)) {
             count++;
+            // Console.WriteLine($"{r}, {c}");
+        }
         
         // N
-        if(wordSearch[r-1,c] == 'M' && findXmasHelper(r-1, c, 1, Direction._N, wordSearch, rowLen, colLen))
+        if(wordSearch[r-1,c] == 'M' && findXmasHelper(r-1, c, 1, Direction._N, wordSearch, rowLen, colLen)) {
             count++;
+            // Console.WriteLine($"{r}, {c}");
+        }
         
         // NE
-        if(c<colLen-1 && wordSearch[r-1,c+1] == 'M' && findXmasHelper(r-1, c+1, 1, Direction._NE, wordSearch, rowLen, colLen))
+        if(c<colLen-1 && wordSearch[r-1,c+1] == 'M' && findXmasHelper(r-1, c+1, 1, Direction._NE, wordSearch, rowLen, colLen)) {
             count++;
+            // Console.WriteLine($"{r}, {c}");
+        }
     }
 
     // E
-    if(c<colLen-1 && wordSearch[r,c+1] == 'M' && findXmasHelper(r, c+1, 1, Direction._E, wordSearch, rowLen, colLen))
+    if(c<colLen-1 && wordSearch[r,c+1] == 'M' && findXmasHelper(r, c+1, 1, Direction._E, wordSearch, rowLen, colLen)) {
         count++;
+        // Console.WriteLine($"{r}, {c}");
+    }
     
     if(r<rowLen-1) {
         // SE
-        if(c<colLen-1 && wordSearch[r+1,c+1] == 'M' && findXmasHelper(r+1, c+1, 1, Direction._SE, wordSearch, rowLen, colLen))
+        if(c<colLen-1 && wordSearch[r+1,c+1] == 'M' && findXmasHelper(r+1, c+1, 1, Direction._SE, wordSearch, rowLen, colLen)) {
             count++;
+            // Console.WriteLine($"{r}, {c}");
+        }
 
         // S
-        if(wordSearch[r+1,c] == 'M' && findXmasHelper(r+1, c, 1, Direction._S, wordSearch, rowLen, colLen))
+        if(wordSearch[r+1,c] == 'M' && findXmasHelper(r+1, c, 1, Direction._S, wordSearch, rowLen, colLen)) {
             count++;
+            // Console.WriteLine($"{r}, {c}");
+        }
         
         // SW
-        if(c>0 && wordSearch[r+1,c-1] == 'M' && findXmasHelper(r+1, c-1, 1, Direction._SW, wordSearch, rowLen, colLen))
+        if(c>0 && wordSearch[r+1,c-1] == 'M' && findXmasHelper(r+1, c-1, 1, Direction._SW, wordSearch, rowLen, colLen)) {
             count++;
+            // Console.WriteLine($"{r}, {c}");
+        }
     }
 
     // W
-    if(c>0 && wordSearch[r,c-1] == 'M' && findXmasHelper(r, c-1, 1, Direction._W, wordSearch, rowLen, colLen))
+    if(c>0 && wordSearch[r,c-1] == 'M' && findXmasHelper(r, c-1, 1, Direction._W, wordSearch, rowLen, colLen)) {
         count++;
+        // Console.WriteLine($"{r}, {c}");
+    }
     
     return count;
 }
 
-char[,] s= {{'X','M','A','S'}, {'M','W','S', 'A'}, {'A','R','R','M'}, {'S','Q','Q','X'}};
-int rowLen = 4;
-int colLen = 4;
+string? line;
+
+StreamReader sr = new StreamReader("input.txt");
+line = sr.ReadLine();
+
+int rowLen = File.ReadLines("input.txt").Count();
+int colLen = (line is not null) ? line.Length : 0;
+char[,] wordSearch = new char[rowLen,colLen];
+
 
 int count = 0;
+int row = 0;
+try {
+    sr = new StreamReader("input.txt");
+    line = sr.ReadLine();
+    while(line != null) {
+        for(int col=0 ; col<colLen ; col++) {
+            wordSearch[row,col] = line[col];
+        }
+
+        row++;
+        line = sr.ReadLine();
+    }
+}
+catch (Exception e) {
+    Console.WriteLine($"Exception: {e}");
+}
+
 for (int r=0 ; r<rowLen ; r++) {
     for (int c=0 ; c<colLen ; c++) {
-        if(s[r,c] == 'X')
-            count += findXmas(r, c, s, rowLen, colLen);
+        // Console.Write($"{wordSearch[r,c]} ");
+        if(wordSearch[r,c] == 'X')
+            count += findXmas(r, c, wordSearch, rowLen, colLen);
     }
 }
 
-Console.WriteLine(count);
+Console.WriteLine($"Count: {count}");
 
 enum Direction {
     _N, _NE, _E, _SE, _S, _SW, _W, _NW
